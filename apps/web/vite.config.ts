fbcc073c45fac55c3df31926b82fc5a299a81b04
Antoine Lelong <azenoxe@gmail.com>
2026-03-05 21:14:34 +0100

    feat(chore): setup turborepo

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,11 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), tanstackStart(), viteReact()],
+  server: {
+    port: 3000,
+  },
+});
