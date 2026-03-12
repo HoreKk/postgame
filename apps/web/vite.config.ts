6f15fcd0ae013e1cffe77e9beab5b266fcadbc55
Antoine Lelong <azenoxe@gmail.com>
2026-03-12 21:48:46 +0100

    feat (chore): upgrade to vite v8 and nitro beta tag

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,11 +1,11 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), nitro(), tsconfigPaths(), viteReact()],
+  plugins: [tanstackStart(), nitro(), viteReact()],
+  resolve: { tsconfigPaths: true },
   server: {
     port: Number(process.env.PORT) || 3000,
     allowedHosts: true,
