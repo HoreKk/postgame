11123172cf4bbffb8a1ec570be27148307c24bc0
Antoine Lelong <azenoxe@gmail.com>
2026-03-14 10:43:12 +0100

    fix: re-add check for database_url env variable in drizzle config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "drizzle-kit";
 
-// if (!process.env.DATABASE_URL) {
-//   const dotenv = await import("dotenv");
-//   const path = await import("path");
-//   const { fileURLToPath } = await import("url");
-//   const __dirname = path.dirname(fileURLToPath(import.meta.url));
-//   dotenv.config({ path: path.resolve(__dirname, "../../apps/web/.env") });
-// }
-
-console.log(process.env);
+if (!process.env.DATABASE_URL) {
+  const dotenv = await import("dotenv");
+  const path = await import("path");
+  const { fileURLToPath } = await import("url");
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  dotenv.config({ path: path.resolve(__dirname, "../../apps/web/.env") });
+}
 
 export default defineConfig({
   schema: "./src/schema/index.ts",
