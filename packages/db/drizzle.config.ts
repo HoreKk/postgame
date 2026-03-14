dfe2209638b96d7a67a4bb7201bc74cbb0c737be
Antoine Lelong <azenoxe@gmail.com>
2026-03-14 03:45:48 +0100

    fix: test drizzle config for railway issue

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL) {
-  const dotenv = await import("dotenv");
-  const path = await import("path");
-  const { fileURLToPath } = await import("url");
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  dotenv.config({ path: path.resolve(__dirname, "../../apps/web/.env") });
-}
+// if (!process.env.DATABASE_URL) {
+//   const dotenv = await import("dotenv");
+//   const path = await import("path");
+//   const { fileURLToPath } = await import("url");
+//   const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//   dotenv.config({ path: path.resolve(__dirname, "../../apps/web/.env") });
+// }
+
+console.log(process.env);
 
 export default defineConfig({
   schema: "./src/schema/index.ts",
   out: "./src/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "",
+    url: process.env.DATABASE_URL!,
   },
 });
